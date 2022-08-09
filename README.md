ebff7c4720379c6dc136e60c3634c482cc76770d
MinJunKim21 <jinju8012@gmail.com>
2022-08-09 15:59:50 +0900

    nodejs로 express mongoDB 셋팅중

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ FE
 - Next JS
 - Tailwind
 
-뭔가 잘 못 건드림...
+BE
+
+- MongoDB
+- Express
---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dontenv.config();
+
+app.use('/minjun', (req, res) => {
+  console.log('this is minjun web');
+});
+//이렇게 작성하면, localhost:5001/minjun 들어가면 this is minjun web 이라는 콘솔 작동함
+
+app.listen('5001', () => {
+  console.log('backenddaaadddd');
+});
+
+// npm init 으로 package.json 을 만들고,
+// npm i express mongoose dotenv multer 모두 설치
+// package.json 에서 main을 index.js 로 바꾸고
+// index.js 파일도 이렇게 내용 작성하고,
+// npm run start 하면 작동되고,
+// 그런데 오류 뜨는 경우 있는데, localhost5001로 바꾸면 해결됨
+// npm i nodemon 하면 console.log 내용이 바뀌면 저절로 업뎃되어서 실행됨
